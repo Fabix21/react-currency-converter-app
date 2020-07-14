0ddfff332262d6095261988902b964f6985b00db
Fabian Hoszowski <fabix43@gmail.com>
2020-07-14 17:10:11 +0200

    Created select and value box in CurrencyRow class

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function CurrencyRow() {
     return (
         <div>
-            <h2>Currency Row</h2>
+            <input type="numver" />
+            <select>
+                <option value="test">Test</option>
+            </select>
         </div>
     )
 }
